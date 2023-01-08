fef953d24824ebae1f3966aa30612f09af015ff4
JDonMc <donkeycon@protonmail>
2023-01-08 23:17:15 +1100

    forgot about index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const { NeuralNet } = require('./neuralnets.js');
+const { Layer } = require('./layers.js');
+const { SubLayer } = require('./sublayers.js');
+const { Neuron } = require('./neurons.js');
+const { Connection } = require('./connections.js');
+
+
+module.exports { NeuralNet, Layer, SubLayer, Neuron, Connection }
